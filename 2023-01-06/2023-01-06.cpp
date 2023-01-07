b7b81bf7b1da9e42b469008425a4c45fdea24d4e
ongsiru <gusdnr3998@naver.com>
2023-01-08 00:24:32 +0900

    초기 커밋

--- 2023-01-06/2023-01-06.cpp
+++ 2023-01-06/2023-01-06.cpp
@@ -1,20 +1,75 @@
-﻿// 2023-01-06.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#define MAX 987654321
 
-#include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello Worldssssss!\n";
+int n, p, k;
+int answer = MAX;
+
+vector<vector<pair<int, int> > > edges;
+int minPassed[1001];
+
+bool dijkstra(int target) {
+
+    for (int i = 1; i <= n; i++) minPassed[i] = MAX;
+
+    priority_queue<pair<int, int> > pq;
+    pq.push({ 0, 1 });
+    minPassed[1] = 0;
+
+    while (!pq.empty()) {
+
+        int here = pq.top().second;
+        int passed = -pq.top().first;
+        pq.pop();
+
+        if (minPassed[here] > passed) continue;
+
+        for (int i = 0; i < edges[here].size(); i++) {
+            int next = edges[here][i].first;
+            int next_passed = passed + (edges[here][i].second > target);
+
+            if (next_passed < minPassed[next]) {
+                minPassed[next] = next_passed;
+                pq.push({ -next_passed, next });
+            }
+        }
+    }
+
+    return minPassed[n] <= k;
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+int main(void) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+    cin >> n >> p >> k;
+    edges.resize(n + 1);
+
+    int u, v, c;
+    int maxNum = 0;
+    for (int i = 0; i < p; i++) {
+        cin >> u >> v >> c;
+        edges[u].push_back({ v, c });
+        edges[v].push_back({ u, c });
+        maxNum = max(maxNum, c);
+    }
+
+
+    int left = 0, right = maxNum, mid;
+    while (left <= right) {
+        mid = (left + right) / 2;
+        if (dijkstra(mid)) {
+            right = mid - 1;
+            answer = mid;
+        }
+        else {
+            left = mid + 1;
+        }
+    }
+
+    if (answer == MAX) cout << -1;
+    else cout << answer;
+}
