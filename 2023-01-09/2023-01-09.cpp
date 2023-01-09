161034e57901b04e82ad069e6dc5ec889387a60f
ongsiru <gusdnr3998@naver.com>
2023-01-10 01:41:58 +0900

    초기 커밋

---
+++ 2023-01-09/2023-01-09.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string S, temp;
+    int len, i;
+
+    cin >> S;
+    len = S.length();
+
+    temp.push_back(S[0]);
+    for (i = 1; i < len; i++)
+    {
+        if (temp[0] >= S[i])
+        {
+            reverse(temp.begin(), temp.end());
+            temp.push_back(S[i]);
+            reverse(temp.begin(), temp.end());
+        }
+        else
+        {
+            temp.push_back(S[i]);
+        }
+    }
+
+    cout << temp;
+
+    return 0;
+}
