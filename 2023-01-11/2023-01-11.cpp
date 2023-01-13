b4f6ef31a5a064b3c3aa051f57c991cca75782e7
ongsiru <gusdnr3998@naver.com>
2023-01-13 10:30:17 +0900

    초기 커밋

---
+++ 2023-01-11/2023-01-11.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+#define MAX 9
+
+int N, M;
+int arr[MAX];
+
+void dfs(int num, int k) {
+    if (k == M) { // M까지 들어갔을 시 실행
+        for (auto i = 0; i < M; i++)
+            cout << arr[i] << " ";
+        cout << "\n";
+    }
+    else { // M까지 안 들어갔을 시
+        for (auto i = num; i <= N; i++) {
+            arr[k] = i; // 값 저장
+            dfs(i + 1, k + 1); // 더 깊게 내려가자 (M까지)
+        }
+    }
+}
+
+int main() {
+    cin >> N >> M;
+    dfs(1, 0);
+}
---
+++ 2023-01-12/2023-01-12.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+#define MAX 9
+
+int N, M;
+int arr[MAX];
+bool visited[MAX];
+
+void dfs(int k) {
+    if (k == M) { // M까지 들어갔을 시 실행
+        for (auto i = 0; i < M; i++)
+            cout << arr[i] << " ";
+        cout << "\n";
+    }
+    else { // M까지 안 들어갔을 시
+        for (auto i = 1; i <= N; i++) { //1부터!
+            arr[k] = i; // 값 저장
+            dfs(k + 1); // 더 깊게 내려가자 (M까지)
+        }
+    }
+}
+
+int main() {
+    cin >> N >> M;
+    dfs(0);
+}
