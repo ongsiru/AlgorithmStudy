7fca7263b648383a5c7f40d0881528050c9f6537
ongsiru <gusdnr3998@naver.com>
2023-01-25 13:06:39 +0900

    초기 커밋

---
+++ 2023-01-24/2023-01-24.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+#define MAX 9
+
+int n, m;
+int arr[MAX];
+int vis[MAX];
+vector<int> num;
+void func(int k, int idx) {
+    if (k == m)
+    {
+        for (int i = 0; i < k; i++)cout << arr[i] << " ";
+        cout << "\n";
+    }
+    else {
+        int prev = 0;
+        for (int i = idx; i < num.size(); i++) {
+            if (prev != num[i]) {
+                arr[k] = num[i];
+                prev = num[i];
+                func(k + 1, i);
+            }
+        }
+    }
+}
+
+
+int main() {
+    cin >> n >> m;
+    while (n--) {
+        int in; cin >> in;
+        num.push_back(in);
+    }
+    sort(num.begin(), num.end());
+    func(0, 0);
+    return 0;
+}
---
+++ 2023-01-25/2023-01-25.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+	int n;
+	int box[1001];
+	int dp[1001];
+	cin >> n;
+	int ans = 1;
+
+	for (int i = 0; i < n; i++) {
+		cin >> box[i];
+		dp[i] = 1;
+	}
+
+
+	for (int i = 1; i < n; i++) {
+		int maxi = 0;
+		for (int j = 0; j < i; j++) {
+			if (box[j] < box[i]) {
+				if (dp[j] > maxi) {
+					maxi = dp[j];
+				}
+			}
+		}
+		dp[i] += maxi;
+		ans = max(dp[i], ans);
+	}
+
+	cout << ans;
+
+
+	return 0;
+}
